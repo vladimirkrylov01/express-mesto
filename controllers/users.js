eb2dfe710377df742a753a5742756a59a7981b6e
Vladimir <vladimirkrylov@gmail.com>
2022-01-26 14:25:28 +0300

    - Создан контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const HTTP_CODES = require('../utils/response.codes');
+const TOKEN_SECRET = require('../utils/secret');
 
 async function getAllUsers(req, res) {
   try {
@@ -40,16 +43,24 @@ async function getUserById(req, res) {
 }
 
 async function createNewUser(req, res) {
-  const { name, about } = req.body;
+  const {
+    name, about, avatar, password, email,
+  } = req.body;
 
   try {
-    const newUser = await User.create({ name, about });
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name, about, password: hashPassword, email, avatar,
+    });
 
     return res
       .status(HTTP_CODES.SUCCESS_CREATED_CODE)
       .json({
         name: newUser.name,
         about: newUser.about,
+        email: newUser.email,
+        avatar: newUser.avatar,
       });
   } catch (e) {
     if (e.name === 'MongoServerError' && e.code === 11000) {
@@ -132,6 +143,55 @@ async function updateAvatar(req, res) {
   }
 }
 
+async function login(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    const matchingUser = await User.findOne({ email }).select('+password');
+    if (!matchingUser) {
+      return res
+        .status(HTTP_CODES.UNAUTHORIZED)
+        .send({ mesage: 'Неправильны почта или пароль' });
+    }
+
+    const isSame = await bcrypt.compare(password, matchingUser.password);
+
+    if (!isSame) {
+      return res
+        .status(HTTP_CODES.UNAUTHORIZED)
+        .send({ mesage: 'Неправильны почта или пароль' });
+    }
+
+    const token = jwt.sign(
+      { _id: matchingUser._id },
+      TOKEN_SECRET,
+      { expiresIn: '7d' },
+    );
+
+    return res
+      .cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+      })
+      .json({
+        _id: matchingUser._id,
+        name: matchingUser.name,
+        about: matchingUser.about,
+        avatar: matchingUser.avatar,
+        email: matchingUser.email,
+      });
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res
+        .status(HTTP_CODES.BAD_REQUEST_ERROR_CODE)
+        .send({ mesage: 'Переданы некорректные данные' });
+    }
+
+    return next(e);
+  }
+}
+
 module.exports = {
-  getAllUsers, getUserById, createNewUser, updateProfile, updateAvatar,
+  getAllUsers, getUserById, createNewUser, updateProfile, updateAvatar, login,
 };
